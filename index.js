d8b113c5c2dfaa0bc6f354c958c11a4fda8b84a0
Bakhita87 <bakhitaotieno@gmail.com>
2023-07-09 00:07:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    .catch(error => console.log(error));
   
 }
+ 
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -12,6 +19,16 @@ function renderBooks(books) {
   });
 }
 
+
+
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
+
+  
+
+  
 });
+
+
+
+
